497403f8270125c95f9e7ca3ff787084df982934
bermeom <bermeom@javeriana.edu.co>
2015-04-02 12:07:23 -0500

    Agrego clase utils

--- Project_system_intelligent/Nummernerkennung/src/Mundo/Erkennung_Nummern.java
+++ Project_system_intelligent/Nummernerkennung/src/Mundo/Erkennung_Nummern.java
@@ -5,6 +5,7 @@
  */
 package Mundo;
 
+import static Utilities.Utilities.generatorBitSetRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
@@ -22,64 +23,7 @@ public class Erkennung_Nummern {
      * @param args the command line arguments
      */
     
-    public static Random random() {
-       return new Random((long) (Math.random()*(new GregorianCalendar()).getTimeInMillis()));
-    }
     
-    public static Boolean cast_A_ByteBit(byte b) {
-        if (b==0){
-            return false;
-        }
-        return true;
-    }
-    
-    public static BitSet  generatorBitSetRandom(int nbits){
-        int size=nbits/8;
-        //If n is not a multiple of 8
-        if((float)size<(float)nbits/8){
-            size++;
-        }
-        byte []bytes= new byte[size];
-        Random r=random();
-        r.nextBytes(bytes);
-        BitSet bitSet=new BitSet(nbits);
-        
-        for (int i=0,k=0;i<size&&k<nbits;i++){
-            for (int j=0;j<8&&k<nbits;j++,k++){
-                bitSet.set(k,cast_A_ByteBit((byte)((bytes[i]>>j)&1)));
-            }
-        }
-        return bitSet;
-    }
-    
-    public static List<Integer> generatorListIntegerRandom(int nbits) {
-        String arry=generatorBitSetRandom(nbits)+"";
-        List<Integer> list_integer=new ArrayList<Integer>();
-        for (String s : arry.substring(1, arry.length()-1).split(",")) {
-	    list_integer.add(Integer.parseInt(s.trim()));
-	}
-        return list_integer;
-    }
-    public static BitSet convertBitsetToListInteger(List<Integer> list) {
-        int nbits=list.size()*4;
-        BitSet bitSet=new BitSet(nbits);
-        
-        for (int i=0,k=0;i<list.size()&&k<nbits;i++){
-            for (int j=0;j<4&&k<nbits;j++,k++){
-                bitSet.set(k,cast_A_ByteBit((byte)((list.get(i)>>j)&1)));
-            }
-        }
-        return bitSet;
-    }
-    
-    public static void printBitset(BitSet bs,int nbits) {
-        for (int i=nbits;i>=0;){
-            for (int j=0; j<4 && i>=0;j++,i--){
-                System.out.print((bs.get(i)?1:0));
-            }
-            System.out.print(" ");
-        }
-    }
     
     public static void main(String[] args) {//10000000
         byte []bytes= new byte[20];
@@ -94,12 +38,16 @@ public class Erkennung_Nummern {
         System.out.println(r.nextLong());
         System.out.println("----------------------------------");
         BitSet bs=generatorBitSetRandom(9);
-        List<Integer> list=generatorListIntegerRandom(9);
-        BitSet bs1=convertBitsetToListInteger(list);
+        List<Integer> list=Utilities.Utilities.generatorListIntegerRandom(9);
+        BitSet bs1=Utilities.Utilities.convertBitsetToListInteger(list);
         System.out.println(bs);
         System.out.println(list);
         System.out.println(bs1);
-        printBitset(bs1, list.size()*4);
+        Utilities.Utilities.printBitset(bs1, list.size()*4);
+        Utilities.Utilities.printBitset(bs, 9);
+        String routeBase="\\home\\angel\\Documentos\\git\\Sistemas inteligentes\\system-intelligent\\Project_system_intelligent\\DataSet";
+        routeBase="/home/angel/Documentos/git/Sistemas inteligentes/system-intelligent/Project_system_intelligent/DataSet";
+        Utilities.Utilities.readFile(routeBase+"/optdigits.tra");
         
     }
     
